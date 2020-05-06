a443f4ef28eeccea583f05cc352c6a542181cdf2
dmdylan <dylandanowski@gmail.com>
2020-05-06 19:28:42 -0400

    Working on tower placement. Can currently use placeholder tower model to view placement before spawning acutal structure. Need to work on rotation before spawning actual tower

--- Assets/Scripts/Player/StructurePlacement.cs
+++ Assets/Scripts/Player/StructurePlacement.cs
@@ -4,30 +4,81 @@ using UnityEngine;
 
 public class StructurePlacement : MonoBehaviour
 {            
-    [SerializeField] private GameObject structurePrefab;
-    // Generate a ray from the cursor position
+    [SerializeField] private GameObject structurePrefab = null;
+    [SerializeField] private GameObject[] structurePlaceholderModel = null;
+    private GameObject player = null;
+    private GameObject tempStructureHolder;
+    private int structureToPlace = 0;
+    private float mouseX = 0;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        mouseX = Input.GetAxis("Mouse X");
+        if (Input.GetKeyDown(KeyCode.Alpha1) && structureToPlace.Equals(0)) 
         {
-            SpawnStructure();
+            structureToPlace = 1;
+            SpawnPlaceholder(structurePlaceholderModel[0]);
+        }
+
+        switch(structureToPlace)
+        {
+            case 1:
+                PreviewPlacement(tempStructureHolder);
+                break;
+
+            default:
+                break;
         }
     }
 
-    private void SpawnStructure()
+    private void PreviewPlacement(GameObject previewObject)
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var objectIsPlaced = false;
+        var stop = false;
 
-        GameObject barricade = Instantiate(structurePrefab);
+        if(Input.GetMouseButtonDown(0)) 
+            objectIsPlaced = true;
+        if (Input.GetMouseButtonDown(1))
+            stop = true;
 
-        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, 5f))
         {
-            barricade.transform.position = hitInfo.point;
-            barricade.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-        }
+            previewObject.SetActive(true);
+            previewObject.transform.position = hitInfo.point;
+            previewObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
 
-        barricade.transform.rotation = Quaternion.Slerp(Quaternion.Euler(0, barricade.transform.rotation.y,0), Quaternion.Euler(0,ray.origin.y,0), .5f);
+        }
+        else
+            previewObject.SetActive(false);
+               
+        if (stop.Equals(true))
+        {
+            structureToPlace = 0;
+            Destroy(previewObject);
+            return;
+        }
+        
+        if (objectIsPlaced.Equals(true))
+        {
+            previewObject.transform.position = previewObject.transform.position;
+            //previewObject.transform.rotation = Quaternion.Euler(0, mouseX * 5, 0);
+            structureToPlace = 0;
+            return;              
+        }
+    }
+
+    GameObject SpawnPlaceholder(GameObject placeholderStructureToSpawn)
+    {
+        tempStructureHolder = Instantiate(placeholderStructureToSpawn);
+        tempStructureHolder.SetActive(false);
+
+        return tempStructureHolder;
     }
 }
