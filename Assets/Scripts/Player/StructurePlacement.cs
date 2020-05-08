54b6d7a060774718fef94bfe828442a7543e889d
dmdylan <dylandanowski@gmail.com>
2020-05-07 21:45:44 -0400

    Placeholder structure now rotates with camera movement and then spawns in the correct tower

--- Assets/Scripts/Player/StructurePlacement.cs
+++ Assets/Scripts/Player/StructurePlacement.cs
@@ -1,20 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class StructurePlacement : MonoBehaviour
 {            
-    [SerializeField] private GameObject structurePrefab = null;
+    [SerializeField] private GameObject[] structurePrefabs = null;
     [SerializeField] private GameObject[] structurePlaceholderModel = null;
-    private GameObject player = null;
+    private GameObject structureHolder;
     private GameObject tempStructureHolder;
     private int structureToPlace = 0;
     private float mouseX = 0;
-
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,18 +19,39 @@ public class StructurePlacement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1) && structureToPlace.Equals(0)) 
         {
             structureToPlace = 1;
+            structureHolder = structurePrefabs[0];
             SpawnPlaceholder(structurePlaceholderModel[0]);
         }
 
         switch(structureToPlace)
         {
             case 1:
-                PreviewPlacement(tempStructureHolder);
+                PreviewPlacement(tempStructureHolder);             
                 break;
-
+            case 10:
+                PreviewRotation(tempStructureHolder);
+                break;
             default:
                 break;
         }
+    }
+
+    private void PreviewRotation(GameObject previewObject)
+    {
+        previewObject.transform.RotateAround(previewObject.transform.position, previewObject.transform.up, mouseX * 20);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnStructure(structureHolder, previewObject);
+            Destroy(previewObject);
+            structureToPlace = 0;
+        }
+        
+        if (Input.GetMouseButtonDown(1))
+        {
+            structureToPlace = 0;
+            Destroy(previewObject);
+        }        
     }
 
     private void PreviewPlacement(GameObject previewObject)
@@ -43,34 +60,30 @@ public class StructurePlacement : MonoBehaviour
         var objectIsPlaced = false;
         var stop = false;
 
-        if(Input.GetMouseButtonDown(0)) 
-            objectIsPlaced = true;
-        if (Input.GetMouseButtonDown(1))
-            stop = true;
-
-        if(Physics.Raycast(ray, out RaycastHit hitInfo, 5f))
+        if (Input.GetMouseButtonDown(0) && Physics.Raycast(ray, 5f)) { objectIsPlaced = true; }
+        if (Input.GetMouseButtonDown(1)) { stop = true; }
+            
+        if (objectIsPlaced.Equals(true))
         {
-            previewObject.SetActive(true);
-            previewObject.transform.position = hitInfo.point;
-            previewObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
-
+            previewObject.transform.position = previewObject.transform.position;
+            structureToPlace = 10;
         }
         else
-            previewObject.SetActive(false);
-               
+        {
+            if(Physics.Raycast(ray, out RaycastHit hitInfo, 5f))
+            {
+                previewObject.SetActive(true);
+                previewObject.transform.position = hitInfo.point;
+                previewObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+            }
+            else
+                previewObject.SetActive(false);      
+        }
+
         if (stop.Equals(true))
         {
             structureToPlace = 0;
             Destroy(previewObject);
-            return;
-        }
-        
-        if (objectIsPlaced.Equals(true))
-        {
-            previewObject.transform.position = previewObject.transform.position;
-            //previewObject.transform.rotation = Quaternion.Euler(0, mouseX * 5, 0);
-            structureToPlace = 0;
-            return;              
         }
     }
 
@@ -80,5 +93,10 @@ public class StructurePlacement : MonoBehaviour
         tempStructureHolder.SetActive(false);
 
         return tempStructureHolder;
+    }
+
+    void SpawnStructure(GameObject structureToSpawn, GameObject structurePlaceholder)
+    {
+        Instantiate(structureToSpawn, structurePlaceholder.transform.position, structurePlaceholder.transform.rotation);
     }
 }
